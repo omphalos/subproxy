4cb96c5dae05c4a9093e3b366da68af62e763bcf
omph alos <omph.alos@yahoo.com>
2012-07-26 22:36:16 -0500

    Signed-off-by: omph alos <omph.alos@yahoo.com>

--- host.js
+++ host.js
@@ -1,86 +1,15 @@
-process.on('uncaughtException', function(err) { console.error(err); });
+var subdomainProxy = require('./server');
+var argv = require('optimist').argv;
 
-var express = require('express');
-var httpProxy = require('http-proxy');
-var url = require('url');
-var port = 80;
+if(argv.h) {
+	console.log('usage: ');
+	console.log('	-r: host root (example, localhost)');
+	console.log('	-p: port to listen on (example, 80) ');
+	console.log('	-h: this help message');
+} else {
+	var port = argv.p || 80;
+	var subProxyHost = argv.r || 'localhost';
 
-/*
-function createProxy(proxyDomain) {
-	return httpProxy.createServer(
-		function (req, res, proxy) {	
-			console.log('proxying ' + req.headers.host + ' :: ' + proxyDomain);
-			// console.dir(req);		
-			var originalHost = req.headers.host;
-			req.headers.host = proxyDomain;
-			var oldWrite = res.writeHead;
-			res.writeHead = function(statusCode, headers) { 
-				if(headers.location) {
-					console.log(headers.location);	
-					var parsedUrl = url.parse(headers.location);
-					if(parsedUrl.host == req.headers.host) {						
-						parsedUrl.host = 'localhost' + (port === 80 ? '' : ':' + port);			
-						headers.location = url.format(parsedUrl);
-					}
-				}
-				oldWrite.apply(res, arguments);
-			};
-			proxy.proxyRequest(req, res, {
-				host: proxyDomain,
-				port: 80
-			});
-		}
-	);
-};*/
-
-//var proxyDomain = 'en.wikipedia.org';
-//console.log('proxy domain is ' + proxyDomain);
-//createProxy(proxyDomain).listen(port);
-
-var proxy = new httpProxy.HttpProxy({
-	target: {
-		host: 'localhost',
-		port: port
-	}
-});
-
-var thisHost = 'localhost';
-var app = express.createServer();
-
-http.createServer(function (req, res) {
-}).listen(1337, '127.0.0.1');
-
-app.get('*', function(req, res, next) { 
-	console.log('wildcard match:' + req.url);
-	console.log('req.headers.host: ' + req.headers.host);	
-	if(req.headers.host.indexOf(thisHost, this.length - thisHost.length) !== -1) {
-		var proxyDomain = req.headers.host.slice(0, this.length - thisHost.length - 1);
-		console.log('proxy is ' + proxyDomain);
-		var originalHost = req.headers.host;
-		req.headers.host = proxyDomain;
-		var oldWrite = res.writeHead;
-		res.writeHead = function(statusCode, headers) { 
-			if(headers && headers.location) {
-				console.log(headers.location);	
-				var parsedUrl = url.parse(headers.location);
-				if(parsedUrl.host == req.headers.host) {
-					parsedUrl.host = 'en.wikipedia.org.localhost' + (port === 80 ? '' : ':' + port);			
-					headers.location = url.format(parsedUrl);
-				}
-			}
-			oldWrite.apply(res, arguments);
-		};
-		proxy.proxyRequest(req, res, {
-			host: proxyDomain,
-			port: port
-		});
-	} else {
-		console.warn('unexpected host name: ' + req.headers.host);
-		// TODO: put http error here
-		// TODO: test htis error
-	}
-	next();
-});
-app.listen(port);
-
-console.log('listening');
+	subdomainProxy.createServer(port, subProxyHost);
+	console.log('host ' + subProxyHost + ' listening on port ' + port);
+}
--- server.js
+++ server.js
@@ -1,9 +1,42 @@
-var app = require('express');
+var httpProxy = require('http-proxy');
+var url = require('url');
+var http = require('http');
 
-exports.createServer = function() {
-	app.get('*', function(req, res) {
-	var result = app.createServer().
-		use(express.vhost('m.mysite.com', require('/path/to/m').app).
-		use(express.vhost('sync.mysite.com', require('/path/to/sync').app);		
-	return result;
+exports.createServer = function(port, subProxyHost, options) {	
+	httpProxy.createServer(	
+		function (req, res, proxy) {						
+			if(req.headers.host.indexOf(subProxyHost) == req.headers.host.length - subProxyHost.length) {
+				// infer the proxy domain
+				var proxyDomain = req.headers.host.slice(0, req.headers.host.length - subProxyHost.length -1);
+				var originalHost = req.headers.host;
+				// replace the host in the request
+				req.headers.host = proxyDomain;				
+				// intercept calls to writeHead, so that we write the new location there
+				var oldWriteHead = res.writeHead;				
+				res.writeHead = function(statusCode, headers) {
+					if(headers.location) {
+						// replace the host in the response
+						var parsedUrl = url.parse(headers.location);
+						if(parsedUrl.host == req.headers.host) {						
+							parsedUrl.host = originalHost + (port === 80 ? '' : ':' + port);			
+							headers.location = url.format(parsedUrl);
+						}
+					}
+					// now that we've overwritten the header the node-http-proxy passes, we pass it along
+					oldWriteHead.apply(res, arguments);
+				};
+				if(options && options.onProxying) { options.onProxying(req, res); }
+				proxy.proxyRequest(req, res, {
+					host: proxyDomain,
+					port: 80
+				});
+				if(options && options.onProxied) { options.onProxied(req, res); }
+			}
+			else {
+				res.statusCode = 500;
+				res.setHeader('Content-Type', 'text/plain');
+				res.end('Internal Server Error: failed to identify host "' + req.headers.host + '"; expecting "' + subProxyHost +'"');
+			}
+		}
+	).listen(port);
 };
